93b2700dbf087e8c2044d91d5a5a726fb9040b41
Baker El Muhur <b.elmuhur@gmail.com>
2022-04-07 14:02:29 +0200

    Story 2 - save in the database implemented (I forgot)

--- src/main/java/com/switchfully/eurder/items/ItemRepository.java
+++ src/main/java/com/switchfully/eurder/items/ItemRepository.java
@@ -2,9 +2,15 @@ package com.switchfully.eurder.items;
 
 import org.springframework.stereotype.Repository;
 
+import java.util.concurrent.ConcurrentHashMap;
+
 @Repository
 public class ItemRepository {
+
+    private final ConcurrentHashMap<String, Item> itemsDatabase = new ConcurrentHashMap<>();
+
     public Item saveItem(Item item) {
-        return item;
+        itemsDatabase.put(item.getId(),item);
+        return itemsDatabase.get(item.getId());
     }
 }
